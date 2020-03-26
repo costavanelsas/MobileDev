fa4a6c3da29b3af7fe7eeac19ca88b414265737c
Costa <costa.van.elsas@hva.nl>
2020-03-26 10:47:07 +0100

    level 4 task example upload

---
+++ level-4-task-example/app/src/main/java/com/example/level_4_task_example/AddActivity.kt
@@ -0,0 +1,40 @@
+package com.example.level_4_task_example
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_add.*
+
+const val EXTRA_REMINDER = "EXTRA_REMINDER"
+
+class AddActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+        setSupportActionBar(toolbar)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        fab.setOnClickListener { onSaveClick() }
+    }
+
+
+    private fun onSaveClick() {
+        if (etAddReminder.text.toString().isNotBlank()) {
+            val reminder = Reminder(etAddReminder.text.toString())
+            val resultIntent = Intent()
+            resultIntent.putExtra(EXTRA_REMINDER, reminder)
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+        } else {
+            Toast.makeText(this, R.string.error_message
+                , Toast.LENGTH_SHORT).show()
+        }
+    }
+}
